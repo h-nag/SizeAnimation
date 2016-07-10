1ede5484b3fefd402f6bfc790db7040b00390811
Hilarion <nagasawahidenori@nagasawa-eitou-no-macbook-pro.local>
2016-07-10 22:03:10 +0900

    SizeAnimation creation

--- SizeAnimation/ViewController.swift
+++ SizeAnimation/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let animation = CASpringAnimation(keyPath: "bounds.size")
+    
+    @IBOutlet weak var animationView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // animation start
+        animationView.layer.cornerRadius = 120
+        startAnimation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func startAnimation(){
+        animation.mass = 1.0
+        animation.damping = 12.0
+        animation.stiffness = 240.0
+        
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+        animation.fromValue = NSValue(CGSize: CGSizeMake(0, 0))
+        animation.toValue = NSValue(CGSize: CGSizeMake(240, 240))
+        
+        animation.removedOnCompletion = true
+        animation.fillMode = kCAFillModeForwards
+        animation.duration = 2.5
+        
+        animation.delegate = self
+        
+        animationView.layer.addAnimation(animation, forKey: "animation")
+    }
 }
 
